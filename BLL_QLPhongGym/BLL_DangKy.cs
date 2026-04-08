f54e69e57a807834bb2ed6b68de1b9fc86f29c1d
GioMuaDong2006 <legiakiet7499@gmail.com>
2026-04-08 10:33:43 +0700

    Add files via upload
    Update DK va TK

---
+++ BLL_QLPhongGym/BLL_DangKy.cs
@@ -0,0 +1,29 @@
+﻿using DAL_QLPhongGym;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL_QLPhongGym
+{
+    public class BLL_DangKy
+    {
+        DAL_DangKy dalDK = new DAL_DangKy();
+
+        public DataTable getGoiTap()
+        {
+            return dalDK.getGoiTap();
+        }
+
+        public DataTable getPT()
+        {
+            return dalDK.getPT();
+        }
+        public void ThemMoiHoiVienVaDangKy(string ten, string sdt, int magoi, int mapt)
+        {
+            dalDK.ThemMoiHoiVienVaDangKy(ten, sdt, magoi, mapt);
+        }
+    }
+}
---
+++ BLL_QLPhongGym/BLL_TaiKhoan.cs
@@ -0,0 +1,18 @@
+﻿using DAL_QLPhongGym;
+
+namespace BLL_QLPhongGym
+{
+    public class BLL_TaiKhoan
+    {
+        DAL_TaiKhoan dalTK = new DAL_TaiKhoan();
+
+        public bool Login(string user, string pass)
+        {
+            // Có thể thêm logic kiểm tra trống ở đây
+            if (string.IsNullOrEmpty(user) || string.IsNullOrEmpty(pass))
+                return false;
+
+            return dalTK.checkLogin(user, pass);
+        }
+    }
+}
